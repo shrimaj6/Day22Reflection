b5d1b8807092a3d773fb34e7d5d17688252a0be2
Shrimaj <shrimaj.mehta60@gmail.com>
2021-12-02 13:14:17 +0530

    Add project files.

---
+++ Day22Reflection/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+namespace Day22Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                //for assembly
+                Assembly assembly = typeof(int).Assembly;
+                Console.WriteLine(assembly);
+
+                Type hype = typeof(System.String);
+                Console.WriteLine(hype.Assembly);
+
+
+                //For method
+                Type o = typeof(UserRegistration);
+                MethodInfo[] mi = o.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+
+                foreach (MethodInfo m in mi)
+                {
+                    Console.WriteLine(m);
+
+                }
+            }
+        }
+    }
+}
+
---
+++ Day22Reflection/UserRegistration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day22Reflection
+{
+    class UserRegistration
+    {
+        public static string First_nameuc1 = "^[A-Z]{3}";
+
+
+
+        public bool checking(string name)
+        {
+            return Regex.IsMatch(name, First_nameuc1);
+        }
+
+    }
+}
